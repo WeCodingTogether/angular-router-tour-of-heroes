e4581fe38062a864630d8d41c9996331584462c8
susie <chenpitools@gmail.com>
2023-10-09 13:03:11 +0800

    uodated simple router outlet, wildcard route, default redirect route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+
+const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/heroes'},
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'heroes', component: HeroListComponent },
+  { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroListComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }) //When true, log all internal navigation events to the console. Use for debugging.
   ],
   providers: [],
   bootstrap: [AppComponent]
